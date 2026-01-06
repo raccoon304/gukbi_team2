2dbc1f9effe70c4fceaebe3ab8412cc84cd54b7a
TH\82104 <solee7966@gmail.com>
2026-01-06 18:31:16 +0900

    상품옵션페이지에 로그인조건 추가, 관리자 조건문 추가 01/06 18:31

--- semiproject/src/main/java/product/controller/ProductOption.java
+++ semiproject/src/main/java/product/controller/ProductOption.java
@@ -17,17 +17,15 @@ public class ProductOption extends AbstractController {
 	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		String productCode = request.getParameter("productCode");
-		//위의 받아온 productCode는 상품테이블의 상품코드임!!
-		
 		//로그인을 한 세션정보 가져오기(MemberLogin.java / myPage.java 참고)
 		HttpSession session = request.getSession();
 		MemberDTO loginUser = (MemberDTO) session.getAttribute("loginUser");
 		
-		System.out.println(loginUser.getName());
-		
 		request.setAttribute("loginUser", loginUser);
+
+		
+		String productCode = request.getParameter("productCode");
+		//위의 받아온 productCode는 상품테이블의 상품코드임!!
 		
 		try {
 			//제품정보 가져오기(제품테이블)
