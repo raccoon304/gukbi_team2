b9bcfd576a402a83355f2d578d099011088562d9
sist <kimsy>
2026-01-19 18:10:08 +0900

    01/19 18:10_테스팅 문제점 보완_김서영

--- semiproject/src/main/java/admin/delivery/model/AdminDeliveryDAO_imple.java
+++ semiproject/src/main/java/admin/delivery/model/AdminDeliveryDAO_imple.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
